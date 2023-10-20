0ab4aa2455f35db59f5ae32181dfe2e0b5f8fe68
shivansh13579 <shivansh0975@gmail.com>
2023-10-20 10:23:17 +0530

    create store and authSlice

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -66,4 +66,31 @@ export class Service{
             return false
         }
     }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: getPost :: error",error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error",error);
+            return false;
+
+        }
+    }
 }
